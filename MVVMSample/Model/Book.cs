3b6cb139389127835421fb578c6f1ae1d31d91a4
helic <helic@DSHELIC10>
2019-05-05 05:54:13 +0300

    task 1

---
+++ MVVMSample/Model/Book.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.ComponentModel;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MVVMSample.Model
+{
+    public class Book : INotifyPropertyChanged
+    {
+        string author;
+        string title;
+
+        public string Author {
+            get { return author; }
+            set {
+                author = value;
+                OnPropertyChanged("Author");
+            }
+        }
+
+        public string Title
+        {
+            get { return title; }
+            set
+            {
+                title = value;
+                OnPropertyChanged("Title");
+            }
+        }
+        void OnPropertyChanged(string PropertyName) {
+            if (PropertyChanged != null)
+                PropertyChanged(this, new PropertyChangedEventArgs(PropertyName));
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+       
+    }
+}
---
+++ MVVMSample/ViewModel/MainWindowViewModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.ComponentModel;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MVVMSample.Model;
+
+namespace MVVMSample.ViewModel
+{
+    public class MainWindowViewModel : INotifyPropertyChanged
+    {
+        Book[] books;
+        public Book[] Books { get; private set; }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        void OnPropertyChanged(string PropertyName)
+        {
+            if (PropertyChanged != null)
+                PropertyChanged(this, new PropertyChangedEventArgs(PropertyName));
+        }
+
+    }
+}
